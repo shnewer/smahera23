67ba63339abb335b611679ea8afa1addd6d4f80f
shnewer <69477068+shnewer@users.noreply.github.com>
2020-11-04 14:09:01 -0500

    Create blackbyry.js

---
+++ blackbyry.js
@@ -0,0 +1,22 @@
+$(`<img src="https://l.top4top.io/p_17697as4m1.gif"class="fr" style="width:100%;margin-top:px;">
+<a id="des22" style="width:95px;float:right;margin:-68px 2px 2px;transform:matrix(1, 0, 0, 1, 3, 70);" class="label label label-primary mini fl" target="_blank" href="https://blby1.com/">القوانين</a>
+<a id="des22" style="width:90px;margin:2px;transform:matrix(1, 0, 0, 1, 3,0);" class="label label label-primary mini fl" target="_blank" href="https://blby1.com/">شات جوال</a>
+<a id="des22" style="width:90px;margin:2px;transform:matrix(1, 0, 0, 1, 3,0);" class="label label label-primary mini fl" target="_blank" href="https://blby1.com/">شات الجوال</a>
+<a id="des22" style="color:rgb(255, 217, 0);width:92px;margin:2px;transform:matrix(1, 0, 0, 1, 3,0);" class="label label label-primary mini fl" target="_blank" href="https://www.chat-hi.com/">شات كتابي</a>
+`).insertBefore('.nav-tabs');
+
+$.getScript("https://raw.githack.com/waleed-qaid/2des/master/www.1des.net-chatt-fkamh.com0.js");
+
+$('.dpnl').append(` <div id="mic" style="height: 100%;width:100%;" class="break light tab-pane border"><input type="jfa-w" name="FirstName" value=" قرآن كريم ">
+<audio class="wp-audio-shortcode" id="audio-236-1" preload="none" style="width: 100%;" controls="controls"><source type="audio/mpeg" src="https://h.top4top.io/m_1595scph70.mp3"><a href="https://h.top4top.io/m_1595scph70.mp3">https://h.top4top.io/m_1595scph70.mp3</a></audio>
+<center></center></center>
+</div>`);
+
+$('#d0').append(`<label title="الإذاعه" href="#" onclick="$('.pnhead').text($(this).attr('title'));hl($(this),'primary');setTimeout(function(){$('#mic').scrollTop(0);},100);$('.dpnl').show();"
+data-toggle="tab" data-target="#mic"  class="ae fa label label-primary fa-microphone ">الاذاعه</label>`);
+
+$("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?($(`<input type="jfa-w" name="FirstName" value=" الأخبــــار "><iframe class="fr" style="width:100%;margin-top: 0px;" src="https://www.bbc.com/ws/av-embeds/media/bbc_arabic_radio/liveradio/ar" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div><center>`).appendTo
+("#mic"),clearInterval(b)):console.log(c)},1)});
+
+$("div#tlogins button.btn.btn-primary").click(function(){var b=setInterval(function(){var c=$(".pmsgc").length;0<c?($(`<input type="jfa-w" name="FirstName" value=" أجنبـــــي "><audio src="https://d.top4top.io/m_1673vuf0x1.mp4" controls loop></audio></div><center>`).appendTo
+("#mic"),clearInterval(b)):console.log(c)},1)});
